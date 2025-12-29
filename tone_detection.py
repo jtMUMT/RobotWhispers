cda06e988d7d26ae53dc666caef0cf14f02950e5
Zeph T <zeph.thibodeau@gmail.com>
2025-12-29 12:37:45 +0800

    (tone production): generates tone responses to the input file

--- tone_detection.py
+++ tone_detection.py
@@ -2,11 +2,16 @@ import numpy as np
 from scipy.io import wavfile
 import matplotlib.pyplot as plt
 
-# samplerate, data = wavfile.read('testdata/sin250.wav')
-# samplerate, data = wavfile.read('testdata/sin500.wav')
-# samplerate, data = wavfile.read('testdata/sin500_noise18.wav')
-# samplerate, data = wavfile.read('testdata/sin500_noise0.wav')
-samplerate, data = wavfile.read('testdata/sine500_noise6_multi.wav')
+test_dir = 'testdata/'
+test_files = ['sin250.wav',
+              'sin500.wav',
+              'sin500_noise18.wav',
+              'sin500_noise0',
+              'sine500_noise6_multi',
+              'whistletest_01.wav']
+
+
+samplerate, data = wavfile.read(test_dir + test_files[5])
 
 print(samplerate)
 print(len(data)/samplerate)
@@ -23,11 +28,12 @@ onset_start = None
 note_on = False
 new_note = False
 new_off = False
-offset_level = 0.2
+offset_level = 0.05
 sampdiffs = [0]
 smoothing_factor = 1/100
 start = 0
-end = int(samplerate / 2)
+# end = int(samplerate / 2)
+end = len(data)
 simple_posedgecount = 0
 simple_pitch_estimate = 0
 pitch_scaling = 1000
@@ -36,6 +42,9 @@ last_pos_edge = 0
 pulse_width_pitch_est = 0
 pitch_smoothing_factor = 1/441
 pitch_env_smooth = []
+
+detected_pulses = []
+this_pulse = {}
 
 pulse_start = 0
 pulse_end = 0
@@ -53,10 +62,16 @@ for s in range((end-start)):
     else:
         crossings.append(-1)
 
+    if (s == len(data)-1):
+        new_off = True
+        note_on = False
+
     if (s > 1) and (note_on):
         if (new_note):
+            this_pulse = {}
             new_note = False
             onset_start = s
+            this_pulse['start'] = onset_start
             pitch_env.append(0)
             pitch_env_smooth.append(0)
         else:
@@ -80,8 +95,13 @@ for s in range((end-start)):
     else:
         if (new_off):
             new_off = False
+            this_pulse['end'] = s
+            this_pulse['duration'] = this_pulse['end'] - this_pulse['start']
             median_pitch_est = np.median(pitch_env_smooth[onset_start:s])
-            print(median_pitch_est)
+            this_pulse['pitch_median'] = median_pitch_est
+            # print(median_pitch_est)
+            print(this_pulse)
+            detected_pulses.append(this_pulse)
         pitch_env.append(0)
         pitch_env_smooth.append(0)
 
@@ -105,17 +125,35 @@ for s in range((end-start)):
             else:
                 envelope_binary.append(0)
 
-median_pitch_est = np.median(pitch_env_smooth[onset_start:s])
-print(median_pitch_est)
+# median_pitch_est = np.median(pitch_env_smooth[onset_start:s])
+# print(this_pulse)
 
-plt.figure()
-# plt.plot(data[start:end])
-plt.plot(crossings[start:end],label='cross')
-# plt.plot(rectified[start:end])
-plt.plot(envelope[start:end],label='env')
-plt.plot(envelope_binary[start:end],label='thresh')
-# plt.plot(sampdiffs[start:end])
-plt.plot(np.asarray(pitch_env[start:end])/pitch_scaling,label='pitch')
-plt.plot(np.asarray(pitch_env_smooth[start:end])/pitch_scaling,label='pitch_smooth')
-plt.legend()
-plt.show()
+# RENDER the detected tones as audio
+last_sample = detected_pulses[-1]['end']
+print(last_sample)
+outwav = np.zeros(last_sample)
+
+for p in detected_pulses:
+    s = p['start']
+    e = p['end']
+    d = p['duration']
+    print(s,d)
+    f = p['pitch_median']
+    for i in range(d):
+        outwav[s+i] = np.sin(f * 2 * np.pi *  (i/samplerate))
+        # outwav[s+i] = 1.0
+
+wavfile.write('test_output.wav', samplerate, outwav)
+    
+
+# plt.figure()
+# # plt.plot(data[start:end],label='data')
+# # plt.plot(crossings[start:end],label='cross')
+# # plt.plot(rectified[start:end])
+# plt.plot(envelope[start:end],label='env')
+# plt.plot(envelope_binary[start:end],label='thresh')
+# # plt.plot(sampdiffs[start:end])
+# # plt.plot(np.asarray(pitch_env[start:end])/pitch_scaling,label='pitch')
+# plt.plot(np.asarray(pitch_env_smooth[start:end])/pitch_scaling,label='pitch_smooth')
+# plt.legend()
+# plt.show()
